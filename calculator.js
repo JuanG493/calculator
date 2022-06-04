5cfe08a0d4e534d39686d3e5d3dcc0ef093e3054
JuanD <juanguarin493@gmail.com>
2022-06-04 09:38:39 +0200

    varius operations at the same time, same operation after clik '='

--- calculator.js
+++ calculator.js
@@ -17,13 +17,11 @@ for (let a = 0; a <= elementsNmae.length - 1; a++) {
 
 let buttons = document.querySelectorAll('button');
 buttons.forEach(element => {
-    // element.addEventListener("click", filterVariables);
-    element.addEventListener("click", testing);
+    element.addEventListener("click", calculatorNumbs);
 
 });
 
 let resultFrame = document.querySelector('.output');
-// resultFrame.addEventListener("click", partialResult);
 
 let firstPartialV = "";
 let secondPartialV = "";
@@ -37,32 +35,30 @@ let history = [];
 let partialValue = "";
 let theText = "";
 
-// console.log(`first value`)
-
-// let fisrtValue = "";
-// let secondValue = "";
 
 
 //filters
-function testing() {
+function calculatorNumbs() {
     theText = this.textContent;
-
+    
     tester = this.textContent;
-
+    
     let numValue = parseInt(this.textContent);
     const surchEle = optElements.some((element) => {
         return element == tester;
     })
-    
-    if(theText == '=' && listOfOperators[listOfOperators.length-1]== '='){
+
+    if (theText == '=' && listOfOperators[listOfOperators.length - 1] == '=') {
         partialResult();
     }
-    
+
     else if (surchEle) {
-        if(listOfValues.length >=2){
+        if (listOfValues.length >= 2) {//para quitar el segundo valor si la operacion a contnuar no era la misma anterior presionando el =
             listOfValues.pop();
-            
-        }else{
+            listOfOperators.push(theText);
+
+
+        } else {
 
             listOfOperators.push(this.textContent);
             listOfValues.push(firstPartialV);
@@ -70,91 +66,52 @@ function testing() {
             console.log(history)
             resultFrame.textContent = listOfValues[listOfValues.length - 1];
             firstPartialV = "";
-            // choiseOperation = this.textContent;
-    
+
             if (listOfOperators.length >= 2) {
                 choiseOperation = listOfOperators[listOfOperators.length - 2]
                 partialResult();
-            //     if (this.textContent != '=') {
-    
-            //     } choiseOperation = this.textContent;
-            //     partialResult();
             }
         }
     }
 
 
     else if (numValue >= 0 || this.textContent == '.') {
-        // const surchNum = listOfValues.some((numb) =>{
-        //     return tester == numb
-        // })
+        
         if (listOfValues.length >= 1) {
             resultFrame.textContent = "";
         }
 
         firstPartialV += this.textContent;
         resultFrame.textContent = firstPartialV;
-
-
-
-        // }
-        // else if (this.textContent == '=') {
-
-        //     partialResult();
-
     }
     else if (this.textContent == 'DEL') {
         deleteAll();
     } else if (this.textContent == 'CE') {
         back();
     }
-    // else {
-    //     choiseOperation = this.textContent;
-    //     // console.log(choiseOperation)
-    //     // console.log(firstValue);
 
-    //     secondPartialV = "";
-    //     // resultFrame.textContent = fisrtValue
-    // }
-
-    }
-
-
-
-
-function equealOperator() {
-    resultFrame.textContent = result;
 }
 
+
 function solutionOutp(solution) { //quita el primero y pone el resultaso en su lugar luego borra el segundo para una nueva operacion.
-    if (theText == '=') {
+  
+ if (theText == '=') {
 
         console.log("result: " + solution)
         listOfValues.splice(0, 1, solution.toString(10))
         console.log(listOfValues);
+        
         resultFrame.textContent = solution;
-    }else{
+    } else {
 
         console.log("result: " + solution)
         listOfValues.splice(0, 1, solution.toString(10))
         console.log(listOfValues);
         listOfValues.pop();
         console.log(listOfValues);
-    
+
         resultFrame.textContent = solution;
     }
-
-    // acumulator = result;
-    // secondValue = result;
-    // lastOperator = choiseOperation;
-    // choiseOperation = "";
-    // firstPartialV = solution;
-
-    // console.log("firstValue:" + firstPartialV);
-    // console.log("SeconValue:" + secondPartialV);
-    // console.log("operation: " + choiseOperation)
-    // secondValue = 0;
-    // result = solution;
 
 
 }
@@ -164,10 +121,6 @@ function partialResult() {
 
     let firstValNum = Number(newList[0]);
     let secondValNum = Number(newList[1]);
-    // console.log(typeof (firstValNum));
-    // console.log(secondValue);
-    // console.log(choiseOperation);
-
     switch (choiseOperation) {
         case '+':
             add(firstValNum, secondValNum)
@@ -192,37 +145,43 @@ function partialResult() {
 
     }
 
-
-    // if( acumulator != 0){
-    //     console.log("acumulator: " + acumulator);
-    //     console.log("lastaValue: " +lastValue);
-    //     console.log("operation it's: " + choiseOperation)
-
-    // }
-
 }
 
-
-
-// signPlus.style.gridColumn = 4/4;
-// signPlus.style.backgroundColor = 'black';
-
-//functios 
 
 const deleteAll = function () {
     console.log("deleting");
+    resultFrame.textContent = "";
+
+    listOfOperators = [];
+    listOfValues = [];
+    history = [];
+    firstPartialV = "";
+    secondPartialV = "";
+    choiseOperation = "";
+    result = 0;
+    tester = "";
+
 }
 
 const back = function () {
+    let identi = optElements.some((element) => {
+        return element == history[history.length-1];
+    })
+
     console.log("back");
+    if(identi){
+        listOfOperators.pop();
+    
+    }else{
+        let minusTheLast = firstPartialV.slice(0,-1);
+        firstPartialV = minusTheLast;
+        console.log(firstPartialV);
+        resultFrame.textContent = firstPartialV;
+
+    }
+
 }
 
-const equal = function (a, b) {
-    let operator = listOfOperators[listOfOperators.length - 2];
-    let resultOpt = `${a}${operator}${b}`;
-    solutionOutp(resultOpt)
-
-}
 
 const add = function (a, b) {
     solutionOutp(a + b);
